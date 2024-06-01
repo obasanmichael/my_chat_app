f587c982932045276e9affa02effd56f2768daeb
Obasan Tolulope <obasantolu@gmail.com>
2024-06-01 22:38:53 +0100

    worked on little push notification

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:my_chat_app/widgets/chat_messages.dart';
 import 'package:my_chat_app/widgets/new_messages.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setUpNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    final notificationSettings = await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setUpNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
